a3fd54ca89c55bfa14522deaab9636576725cd1b
Sam <opensource@acadea.io>
2022-05-30 23:33:32 +0800

    added error handling properties

--- app/Jobs/ProcessLocationCsv.php
+++ app/Jobs/ProcessLocationCsv.php
@@ -17,6 +17,28 @@ class ProcessLocationCsv implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    // number of times the job may be attempted
+    public $tries = 5;
+
+    // max num of unhandled exception to allow before failing
+    public $maxExceptions = 3;
+
+    // job is considered as failed on timeout
+    public $failOnTimeout = true;
+    // num of sec the job can run before timing out
+    public $timeout = 10;
+
+    // num of sec to wait before reattempting the job
+    public $backoff = 3;
+
+    public $deleteWhenMissingModels = true;
+
+//    public function backoff()
+//    {
+//        return [2,3,4];
+//    }
+
+
     /**
      * Create a new job instance.
      *
